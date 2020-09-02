96f46c115ad9dce33a89dc5fb433c2dcc2e06505
kyledexperience <kyledexperience@gmail.com>
2020-09-02 10:21:24 -0400

    branch 3

--- src/branchingdemo/Footloose.java
+++ src/branchingdemo/Footloose.java
@@ -6,11 +6,15 @@ public class Footloose {
 
 		System.out.println("It was a dark and stormy night");
 
+		
+		
 		System.out.println(
 				"a bone chilling gust of filth-strewn wind wrapped the loose ends of his open trench coat around him like a day-old flour tortilla around a breakfast");
 
-System.out.println("burrito with hash browns, sausage, and scrambled eggs, hold the pico.");
-	
+		
+		
+		System.out.println("burrito with hash browns, sausage, and scrambled eggs, hold the pico.");
+
 	}
 
 }
